5071c56c0e3e84184de62e7aa8654fa1ea136644
Dara Tejaswi <151639105+daratejaswi1924@users.noreply.github.com>
2023-12-20 20:37:52 +0530

    changes added

--- todo-cli/migrations/20221102032718-create-todo.js
+++ todo-cli/migrations/20221102032718-create-todo.js
@@ -1,14 +1,14 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-    async up(queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Todos', {
-    id: {
+      id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
-    },
+      },
       title: {
         type: Sequelize.STRING
       },
