b3a40e2f8d721945f39fd67c83df2120fcf50b1a
Dara Tejaswi <151639105+daratejaswi1924@users.noreply.github.com>
2024-01-24 14:11:20 +0530

    changes added

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,8 +1,5 @@
 const app = require("./app");
 
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT,()=>{
-    console.log('started express server');
-   
+app.listen(3000, () => {
+  console.log("Started express server at port 3000");
 });
--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -11,21 +9,28 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Todo.belongsTo(models.User, { foreignKey: 'userId' }); // Assuming a User model with a primary key 'id'
+    
     }
-    static addTodo({title,dueDate}){
-      return this.create({title:title,dueDate:dueDate,completed:false})
+
+    static addTodo({ title, dueDate }) {
+      return this.create({ title: title, dueDate: dueDate, completed: false })
     }
+
     markAsCompleted() {
-      return this.update({completed: true})
+      return this.update({ completed: true });
     }
   }
-  Todo.init({
-    title: DataTypes.STRING,
-    dueDate: DataTypes.DATEONLY,
-    completed: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Todo',
-  });
+  Todo.init(
+    {
+      title: DataTypes.STRING,
+      dueDate: DataTypes.DATEONLY,
+      completed: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Todo",
+    }
+  );
   return Todo;
 };
